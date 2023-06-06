1680e05a98fd1752c358d8f6df179148e8a123e8
Chaneun Kim <cksdms977@naver.com>
2023-06-06 22:49:39 +0900

    jsp수정2차

---
+++ _Test/src/main/java/com/web/common/listener/MyContextListener.java
@@ -0,0 +1,41 @@
+package com.web.common.listener;
+
+import javax.servlet.ServletContextEvent;
+
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import com.web.common.AESEncryptor;
+
+
+
+/**
+ * Application Lifecycle Listener implementation class MyContextListener
+ *
+ */
+@WebListener
+public class MyContextListener implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public MyContextListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  { 
+    	
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    	new AESEncryptor(); 
+    }
+	
+}
